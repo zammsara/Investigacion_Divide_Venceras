6f0bdcd31b337b54c65c90c82521cb7a93e0c82a
zammsara <saztaylor@uamv.edu.ni>
2024-10-10 17:02:57 -0600

    Final Commit

--- MergeSort-Ejemplo/MergeSort-Ejemplo/Form1.Designer.cs
+++ MergeSort-Ejemplo/MergeSort-Ejemplo/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.lbCalificaciones = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(73, 61);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(92, 33);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(192, 61);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(92, 33);
+            this.btnOrdenar.TabIndex = 1;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // lbCalificaciones
+            // 
+            this.lbCalificaciones.FormattingEnabled = true;
+            this.lbCalificaciones.Location = new System.Drawing.Point(73, 130);
+            this.lbCalificaciones.Name = "lbCalificaciones";
+            this.lbCalificaciones.Size = new System.Drawing.Size(398, 238);
+            this.lbCalificaciones.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 422);
+            this.Controls.Add(this.lbCalificaciones);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.ListBox lbCalificaciones;
     }
 }
 
--- MergeSort-Ejemplo/MergeSort-Ejemplo/Form1.cs
+++ MergeSort-Ejemplo/MergeSort-Ejemplo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MergeSort_Ejemplo.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace MergeSort_Ejemplo
 {
     public partial class Form1 : Form
     {
+        // Arreglo de calificaciones de estudiantes
+        private int[] calificaciones = { 85, 92, 74, 88, 90, 69, 73, 95, 80, 77 };
+
+        // Instancia de la clase que contiene la logica del Merge Sort
+        private MergeSortLogica mergeSortLogica = new MergeSortLogica();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbCalificaciones.Items.Clear();  // Limpiamos el ListBox antes de cargar las califcaciones
+
+            // Agregamos las calificaciones al ListBox
+            foreach (int calificacion in calificaciones)
+            {
+                lbCalificaciones.Items.Add("Estudiante: " + calificacion);
+            }
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            // Llamamos al metodo MergeSort para ordenar el arreglo
+            mergeSortLogica.MergeSort(calificaciones, 0, calificaciones.Length - 1);
+
+            // Limpiamos el ListBox para mostrar el arreglo ordenado
+            lbCalificaciones.Items.Clear();
+
+            // Agregamos las calificaciones ordenadas al ListBox
+            foreach (int calificacion in calificaciones)
+            {
+                lbCalificaciones.Items.Add("Estudiante: " + calificacion);
+            }
+
+            // Mostramos un mensaje confirmando que las calificaciones fueron ordenadas
+            MessageBox.Show("Las calificaciones han sido ordenadas exitosamente.");
         }
     }
 }

f670c0ce22f59ff24680e0da03f3e16445c2423a
gague-UAM <ggpaiz@uamv.edu.ni>
2024-10-10 20:32:08 -0600

    Primera clase Producto

---
+++ TiendaQuickSort/TiendaQuickSort/models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaQuickSort.models
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        // constructor
+        public Producto(string nombre, double precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        // se sobre escribe ToString para mostrar el nombre y precio en el listbox
+        public override string ToString()
+        {
+            return $"{Nombre} - ${Precio}";
+        }
+    }
+}

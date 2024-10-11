5cad362f8cbc11243269e7c2d8fdd2f71dfce6f0
priscila0621 <pjulieths@uamv.edu.ni>
2024-10-10 19:21:20 -0600

    Final Commit

--- AlgoritmoDeStrassen/AlgoritmoDeStrassen/Form1.Designer.cs
+++ AlgoritmoDeStrassen/AlgoritmoDeStrassen/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbA11 = new System.Windows.Forms.TextBox();
+            this.tbA12 = new System.Windows.Forms.TextBox();
+            this.tbB22 = new System.Windows.Forms.TextBox();
+            this.tbB21 = new System.Windows.Forms.TextBox();
+            this.tbB12 = new System.Windows.Forms.TextBox();
+            this.tbB11 = new System.Windows.Forms.TextBox();
+            this.tbA22 = new System.Windows.Forms.TextBox();
+            this.tbA21 = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbA11
+            // 
+            this.tbA11.Location = new System.Drawing.Point(207, 12);
+            this.tbA11.Name = "tbA11";
+            this.tbA11.Size = new System.Drawing.Size(72, 20);
+            this.tbA11.TabIndex = 0;
+            // 
+            // tbA12
+            // 
+            this.tbA12.Location = new System.Drawing.Point(285, 12);
+            this.tbA12.Name = "tbA12";
+            this.tbA12.Size = new System.Drawing.Size(72, 20);
+            this.tbA12.TabIndex = 1;
+            // 
+            // tbB22
+            // 
+            this.tbB22.Location = new System.Drawing.Point(285, 116);
+            this.tbB22.Name = "tbB22";
+            this.tbB22.Size = new System.Drawing.Size(72, 20);
+            this.tbB22.TabIndex = 2;
+            // 
+            // tbB21
+            // 
+            this.tbB21.Location = new System.Drawing.Point(207, 116);
+            this.tbB21.Name = "tbB21";
+            this.tbB21.Size = new System.Drawing.Size(72, 20);
+            this.tbB21.TabIndex = 3;
+            // 
+            // tbB12
+            // 
+            this.tbB12.Location = new System.Drawing.Point(285, 90);
+            this.tbB12.Name = "tbB12";
+            this.tbB12.Size = new System.Drawing.Size(72, 20);
+            this.tbB12.TabIndex = 4;
+            // 
+            // tbB11
+            // 
+            this.tbB11.Location = new System.Drawing.Point(207, 90);
+            this.tbB11.Name = "tbB11";
+            this.tbB11.Size = new System.Drawing.Size(72, 20);
+            this.tbB11.TabIndex = 5;
+            // 
+            // tbA22
+            // 
+            this.tbA22.Location = new System.Drawing.Point(285, 39);
+            this.tbA22.Name = "tbA22";
+            this.tbA22.Size = new System.Drawing.Size(72, 20);
+            this.tbA22.TabIndex = 6;
+            // 
+            // tbA21
+            // 
+            this.tbA21.Location = new System.Drawing.Point(207, 38);
+            this.tbA21.Name = "tbA21";
+            this.tbA21.Size = new System.Drawing.Size(72, 20);
+            this.tbA21.TabIndex = 7;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(31, 29);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(170, 13);
+            this.lblA.TabIndex = 8;
+            this.lblA.Text = "Ingrese los numeros de la matriz A:";
+            this.lblA.Click += new System.EventHandler(this.lblA_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(31, 106);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(170, 13);
+            this.lblB.TabIndex = 9;
+            this.lblB.Text = "Ingrese los numeros de la matriz B:";
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(147, 155);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(94, 33);
+            this.btnMultiply.TabIndex = 10;
+            this.btnMultiply.Text = "Multiplicar";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(75, 194);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.Size = new System.Drawing.Size(242, 90);
+            this.dgvResult.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(397, 328);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.tbA21);
+            this.Controls.Add(this.tbA22);
+            this.Controls.Add(this.tbB11);
+            this.Controls.Add(this.tbB12);
+            this.Controls.Add(this.tbB21);
+            this.Controls.Add(this.tbB22);
+            this.Controls.Add(this.tbA12);
+            this.Controls.Add(this.tbA11);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbA11;
+        private System.Windows.Forms.TextBox tbA12;
+        private System.Windows.Forms.TextBox tbB22;
+        private System.Windows.Forms.TextBox tbB21;
+        private System.Windows.Forms.TextBox tbB12;
+        private System.Windows.Forms.TextBox tbB11;
+        private System.Windows.Forms.TextBox tbA22;
+        private System.Windows.Forms.TextBox tbA21;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.DataGridView dgvResult;
     }
 }
 
--- AlgoritmoDeStrassen/AlgoritmoDeStrassen/Form1.cs
+++ AlgoritmoDeStrassen/AlgoritmoDeStrassen/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AlgoritmoDeStrassen.Models;
 
 namespace AlgoritmoDeStrassen
 {
@@ -15,6 +17,104 @@ namespace AlgoritmoDeStrassen
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            // Validar los datos antes de proceder
+            if (!ValidarDatos())
+            {
+                return; // Si la validación falla, no continuar
+            }
+
+            // Si los datos son válidos, proceder con la multiplicación
+            int a11 = int.Parse(tbA11.Text);
+            int a12 = int.Parse(tbA12.Text);
+            int a21 = int.Parse(tbA21.Text);
+            int a22 = int.Parse(tbA22.Text);
+
+            int b11 = int.Parse(tbB11.Text);
+            int b12 = int.Parse(tbB12.Text);
+            int b21 = int.Parse(tbB21.Text);
+            int b22 = int.Parse(tbB22.Text);
+
+            int[,] A = new int[,] { { a11, a12 }, { a21, a22 } };
+            int[,] B = new int[,] { { b11, b12 }, { b21, b22 } };
+
+            // Crear una instancia de la clase MatrixMultiplication
+            MatrixMultiplication matrixMultiplication = new MatrixMultiplication();
+
+            // Llamar al algoritmo de Strassen para multiplicar las matrices
+            int[,] resultado = matrixMultiplication.Strassen(A, B);
+
+            // Mostrar el resultado en el DataGridView
+            MostrarResultadoEnDataGridView(resultado);
+        }
+        private void MostrarResultadoEnDataGridView(int[,] result)
+        {
+            // Limpiar el DataGridView antes de mostrar los nuevos datos
+            dgvResult.Rows.Clear();
+            dgvResult.Columns.Clear();
+
+            // Definir las columnas del DataGridView (2 columnas para la matriz 2x2)
+            dgvResult.Columns.Add("Col1", "Col1");
+            dgvResult.Columns.Add("Col2", "Col2");
+
+            // Agregar las filas al DataGridView con los valores de la matriz
+            for (int i = 0; i < 2; i++)
+            {
+                // Crear una nueva fila con los valores de la matriz
+                string[] row = new string[2];
+                for (int j = 0; j < 2; j++)
+                {
+                    row[j] = result[i, j].ToString();
+                }
+
+                // Agregar la fila al DataGridView
+                dgvResult.Rows.Add(row);
+            }
+        }
+        private bool ValidarDatos()
+        {
+            // Verificar que todos los TextBox contengan datos
+            if (string.IsNullOrWhiteSpace(tbA11.Text) ||
+                string.IsNullOrWhiteSpace(tbA12.Text) ||
+                string.IsNullOrWhiteSpace(tbA21.Text) ||
+                string.IsNullOrWhiteSpace(tbA22.Text) ||
+                string.IsNullOrWhiteSpace(tbB11.Text) ||
+                string.IsNullOrWhiteSpace(tbB12.Text) ||
+                string.IsNullOrWhiteSpace(tbB21.Text) ||
+                string.IsNullOrWhiteSpace(tbB22.Text))
+            {
+                MessageBox.Show("Por favor ingresa todos los valores en las matrices.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // Verificar que todos los valores ingresados sean números válidos
+            if (!int.TryParse(tbA11.Text, out _) ||
+                !int.TryParse(tbA12.Text, out _) ||
+                !int.TryParse(tbA21.Text, out _) ||
+                !int.TryParse(tbA22.Text, out _) ||
+                !int.TryParse(tbB11.Text, out _) ||
+                !int.TryParse(tbB12.Text, out _) ||
+                !int.TryParse(tbB21.Text, out _) ||
+                !int.TryParse(tbB22.Text, out _))
+            {
+                MessageBox.Show("Por favor ingresa solo números en las matrices.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
         }
     }
 }
